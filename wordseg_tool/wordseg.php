9e3bcbdb6d1fd3e1c38c1212e69588eab5d87f34
godfather239 <greenday239@sina.com>
2016-09-20 15:46:42 +0800

    add a tool

---
+++ wordseg_tool/wordseg.php
@@ -0,0 +1,40 @@
+<?php
+
+function getCompressContent($url) {
+    try{
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+        $output = curl_exec($ch);
+        $output = json_decode($output, 1);
+        curl_close($ch);
+        return $output;
+    }catch(\Exception $e){
+        var_dump($e->getMessage());
+        return false;
+    }
+}
+
+/***************** Start here **************************/
+$show_tag = (isset($argv[1]) && $argv[1] == 'show_tag');
+while (($line = fgets(STDIN, 1024)) !== false) {
+    $chars = array("\r", "\n");
+    $encoded = rawurlencode(str_replace($chars, "", $line));
+    $url = "http://solr.dev.jumei.com/search/keyword_jumei_com/qa?q={$encoded}&wt=json";
+    $res = getCompressContent($url);
+    if (isset($res['analysis'])) {
+        $str = "";
+        foreach ($res['analysis'] as $word => $postag) {
+            if ($show_tag) {
+                $str .= "{$word}/{$postag} ";
+            } else {
+                $str .= $word . " ";
+            }
+        }
+        $str = substr($str, 0, -1);
+        echo "{$str}\n";
+    }
+}
+?>
