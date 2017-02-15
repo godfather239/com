05d933fb18a9e71ef2a29f93fb9ff11a3531a09e
godfather239 <greenday239@sina.com>
2017-02-15 17:13:05 +0800

    add config

--- ecpm_stat/main.py
+++ ecpm_stat/main.py
@@ -3,6 +3,7 @@
 
 import time
 import sys
+import datetime
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import HiveContext
@@ -10,10 +11,11 @@ from pyspark.storagelevel import StorageLevel
 from pyspark.sql.functions import row_number
 from pyspark.sql.functions import desc
 from pyspark.sql import Window
+from config import CONFIG
 
 def get_order_table(sparkContext, sqlContext):
-    start = '2017-02-08'
-    end = '2017-02-10'
+    start = CONFIG['stat_from'].strftime('%Y-%m-%d')
+    end = CONFIG['stat_to'].strftime('%Y-%m-%d')
     table = sqlContext.sql('''
           select sell_label,product_id as productId,sum(quantity*deal_price) as sales_amount from
           bi_datawarehouse.int_paid_orders where data_date >= '%s' AND data_date <= '%s'
@@ -23,12 +25,13 @@ def get_order_table(sparkContext, sqlContext):
           order by sales_amount desc
     ''' % (start, end))
     table.persist(StorageLevel(True, True, False, False, 1))
-    table.write.saveAsTable('recommend.ecpm_order', mode='overwrite')
+    if CONFIG['do_save_table']:
+        table.write.saveAsTable('recommend.ecpm_order'+CONFIG['table_suffix'], mode='overwrite')
     return table
 
 def get_sensor_table(sparkContext, sqlContext):
-    start = '20170208'
-    end = '20170210'
+    start = CONFIG['stat_from'].strftime('%Y%m%d')
+    end = CONFIG['stat_to'].strftime('%Y%m%d')
     table = sqlContext.sql('''
         SELECT search_word,
                product_id,
@@ -50,13 +53,11 @@ def get_sensor_table(sparkContext, sqlContext):
              (SELECT event_id,
                      search_word,
                      p_material_id,
-                     p_material_link,
                      doc_type
               FROM
                 (SELECT event_id,
                         regexp_extract(p_params, '^(.*?)&(.*?)$', 1) AS search_word,
                         regexp_extract(p_material_id, '(.*p)?(\\\d+).*',2) AS p_material_id,
-                        p_material_link,
                         regexp_extract(p_material_link, '^.*&type=(.*?)&.*', 1) AS doc_type
                  FROM rawdata.event_ros_p1
                  WHERE DAY >= '%s'
@@ -81,36 +82,40 @@ def get_sensor_table(sparkContext, sqlContext):
                  product_id
     ''' % (start, end))
     table.persist(StorageLevel(True, True, False, False, 1))
-    table.write.saveAsTable('recommend.ecpm_sensor', mode='overwrite')
+    if CONFIG['do_save_table']:
+        table.write.saveAsTable('recommend.ecpm_sensor'+CONFIG['table_suffix'], mode='overwrite')
     return table
 
 def main():
-    '''
-    1. stat sensors log, output format: sell_label productId exposure_count
-    2. stat order table, output format: search_word product_id sales_amount
-    3. calculate ecpm, output format: search_word product_id exposure_ecpm click_ecpm
-    '''
-conf = SparkConf().setAppName("SearchECPMStat")
-sc = SparkContext(conf = conf)
-sqlContext = HiveContext(sc)
-sc.setLogLevel("WARN")
+    conf = SparkConf().setAppName("SearchECPMStat")
+    sc = SparkContext(conf = conf)
+    sqlContext = HiveContext(sc)
+    sc.setLogLevel("WARN")
 
-# data clean
-orderTable = get_order_table(sc, sqlContext)
-sensorTable = get_sensor_table(sc, sqlContext)
+    # data clean
+    print ('ECPMStat Start get_order_table')
+    orderTable = get_order_table(sc, sqlContext)
+    print ('ECPMStat Finish get_order_table')
 
-cond = [orderTable['sell_label']==sensorTable['search_word'],orderTable['productId']==sensorTable['product_id']]
-table = orderTable.join(sensorTable, cond, 'inner').\
-                   withColumn('exposure_ecpm', orderTable.sales_amount / sensorTable.exposure_count * 100.0).\
-                   withColumn('click_ecpm', orderTable.sales_amount / sensorTable.click_count * 100.0).\
-                   select('search_word', 'product_id', 'exposure_ecpm', 'click_ecpm')
-# result limit
-table = table.withColumn('row_number', row_number().over(Window.partitionBy("search_word").orderBy(desc("exposure_ecpm"))))
-table = table.filter(table['row_number'] <= 100).\
-              drop('row_number')
-table.write.saveAsTable('recommend.ecpm_final', mode = 'overwrite')
+    print ('ECPMStat Start get_sensor_table')
+    sensorTable = get_sensor_table(sc, sqlContext)
+    print ('ECPMStat Finish get_sensor_table')
 
-print ('ECPMStat finished!############################################')
+    print ('ECPMStat Start calculate ecpm value')
+    cond = [orderTable['sell_label']==sensorTable['search_word'],orderTable['productId']==sensorTable['product_id']]
+    table = orderTable.join(sensorTable, cond, 'inner').\
+                       withColumn('exposure_ecpm', orderTable.sales_amount / sensorTable.exposure_count * 100.0).\
+                       withColumn('click_ecpm', orderTable.sales_amount / sensorTable.click_count * 100.0).\
+                       select('search_word', 'product_id', 'exposure_ecpm', 'click_ecpm')
+    # result limit
+    table = table.withColumn('row_number', row_number().over(Window.partitionBy("search_word").orderBy(desc("exposure_ecpm"))))
+    table = table.filter(table['row_number'] <= CONFIG['product_list_limit_per_query']).\
+                  drop('row_number')
+    if CONFIG['do_save_table']:
+        table.write.saveAsTable('recommend.ecpm_final'+CONFIG['table_suffix'], mode = 'overwrite')
+    print ('ECPMStat Finish calculate ecpm value')
+
+    print ('ECPMStat finished!############################################')
 
 if __name__ == "__main__":
     main()
